b8eb49a951cd641e6e0be536da7dc2069fc1d0a1
maxiuw <maciejwozniak94@gmail.com>
2022-10-31 13:59:04 +0100

    daily comm

--- OSCScripts/ObjectRecieverRos.cs
+++ OSCScripts/ObjectRecieverRos.cs
@@ -27,10 +27,27 @@ public class ObjectRecieverRos : MonoBehaviour {
     public List<int> ids;
     ROSConnection m_Ros;
     public float table_y = 0.64f;
-    // Start is called before the first frame update
+    public float camera_height = 450; // Z of the camera in the world 
+    public float camera_focal_length_logi = 4f; // 378 actually seems more possible, https://horus.readthedocs.io/en/release-0.2/source/scanner-components/camera.html
+    public Dictionary<int,int> label_mapping;
+    int best_n_detectableobject = 0;
+    JObject lastmsg;
+
+    // labels 
+    // bannana 52
+    // apple 53
+    // orange 55 
+    // baseball bat 39 - pen 
+    // vase 86 - bana
 
     void Start () {
         m_Ros = ROSConnection.GetOrCreateInstance();
+        label_mapping = new Dictionary<int, int>();
+        label_mapping[46] = 0; // org bammaa 
+        label_mapping[75] = 0; // org vase, bananna
+        label_mapping[34] = 1; // org bat, pen // 34 baseball bat
+        label_mapping[47] = 2; // org appleselected
+        label_mapping[39] = 3; // bottle  
         m_Ros.Subscribe<StringMsg>("/predictedObjects", DoStuff);
         // osc.SetAddressHandler("/objReciever", OnObjectRecieved);
         // positions = new Stack<Vector3>();
@@ -44,19 +61,39 @@ public class ObjectRecieverRos : MonoBehaviour {
     //     Debug.Log(cam.ScreenToWorldPoint(mousePos));
     // }
 	void DoStuff(StringMsg msg) {
+        
         JObject json = JObject.Parse(msg.data);
         int[] classes = json["classes"].ToObject<int[]>();
-        Debug.Log(msg.data);
-        // double[] bb = json["bb"].ToObject<double[]>();
-        for (int i = 0; i < classes.Length; i++) {
-            if (classes[i] != 3) {
-                OnObjectRecieved(classes[i],json["bb"][i].ToObject<double[]>());
-                // Debug.Log($"I recieved {json["bb"][0].ToObject<double[]>()}");
+        int number_detectableobjects = 0;
+        // count the detectable objects, if there are more then last time 
+        foreach (int c in classes) {
+            try {
+                int _ = label_mapping[c];
+                number_detectableobjects++; // in this message 
+            } catch {
+                Debug.Log("I was not trained on that class");
+                // return;
             }
         }
-        
+        // Debug.Log($"length {classes} length {classes.Length}");
+        // check if new msg has more instances than the old one and if yes, it's a new msg 
+        if (lastmsg is null | number_detectableobjects > best_n_detectableobject) {
+            lastmsg = json;
+            best_n_detectableobject = number_detectableobjects;
+            Debug.Log(best_n_detectableobject);
+
+        }
+        // lastmsg = JObject.Parse(msg.data);
     }
-	
+	public void create_env_OnClick() {
+        int[] classes = lastmsg["classes"].ToObject<int[]>();
+        // Debug.Log(msg.data);
+        // double[] bb = json["bb"].ToObject<double[]>();
+        for (int i = 0; i < classes.Length; i++) {
+            OnObjectRecieved(classes[i],lastmsg["bb"][i].ToObject<double[]>());
+                // Debug.Log($"I recieved {json["bb"][0].ToObject<double[]>()}");
+        }
+    }
     
     // // messages are send and recieved in the following manner: 
     // // 0 - class of the object
@@ -66,11 +103,20 @@ public class ObjectRecieverRos : MonoBehaviour {
     // // 8,9,10 - rgb
     // // 11 - instance idx(?)
 	public void OnObjectRecieved(int objclass, double[] bb) {        
+
+        try {
+            objclass = label_mapping[objclass];
+        } catch {
+            Debug.Log("I was not trained on that class");
+            return;
+        }
         // bb format is  x1,y1,x2,y2 (top left corner, bottom right corner in cv2 python coordinate) 
         float x_cam = (float) (cam.pixelWidth - bb[2] + ((cam.pixelWidth - bb[0]) - (cam.pixelWidth - bb[2]))/2);
         float y_cam = (float) (cam.pixelHeight - bb[3] + ((cam.pixelHeight - bb[1]) - (cam.pixelHeight - bb[3]))/2);
+       
         // Debug.Log($"boxes {cam.pixelWidth - bb[0]}, {cam.pixelHeight - bb[1]}, {cam.pixelWidth - bb[2]}, {cam.pixelHeight - bb[3]}");
         Debug.Log($"coor {x_cam}, {y_cam}. class {objclass}");
+
         // Quaternion rot = Quaternion.Euler(0, roty, 0); // rot just around y axis are allowed
         // // scale 
         // float scaleFactor = message.GetInt(7);
